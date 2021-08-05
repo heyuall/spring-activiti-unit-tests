26a04556e5c79372fb118fa2093fb1ba686b3ef3
heyuall <zoubeidihoussem@gmail.com>
2021-08-06 00:29:33 +0100

    First commit

---
+++ src/main/java/com/workflow/activitiunittests/ActivitiunittestsApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.activitiunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiunittestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiunittestsApplication.class, args);
+    }
+
+}
